d3d9e39e51d6a207b87373e41765e8ef8a4fea3d
Karmel Allison <karmel@google.com>
2018-02-01 10:10:27 -0800

    Debugging performance

--- official/resnet/imagenet_main.py
+++ official/resnet/imagenet_main.py
@@ -138,6 +138,8 @@ def synthetic_input_fn(is_training, data_dir, batch_size, num_epochs=1):
   feature_vals = tf.random_normal(num_examples + input_shape)
   label_vals = tf.random_uniform(
       num_examples, minval=0, maxval=_NUM_CLASSES - 1)
+  label_vals = tf.one_hot(label_vals, _NUM_CLASSES)
+
   dataset = tf.data.Dataset.from_tensor_slices((feature_vals, label_vals))
 
   dataset = dataset.repeat(num_epochs)
